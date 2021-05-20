dfcf97f666666e113c98bb3cad0c692710e24640
Eduardo <eduardomartinezoc@gmail.com>
2021-05-20 10:59:45 +0200

    test 4 pasado correctamente

--- test/operaciones/OperacionesTest.java
+++ test/operaciones/OperacionesTest.java
@@ -71,7 +71,7 @@ public class OperacionesTest {
     /**
      * Test of mayor method, of class Operaciones.
      */
-    
+    @Ignore
     @Test
     public void testMayorA() {
         System.out.println("mayor A");
@@ -86,18 +86,16 @@ public class OperacionesTest {
     /**
      * Test of mayor method, of class Operaciones.
      */
-    @Ignore
+    
     @Test
     public void testMayorB() {
         System.out.println("mayor B");
-        int a = 0;
-        int b = 0;
+        int a = 2;
+        int b = 3;
         Operaciones instance = new Operaciones();
-        int expResult = 0;
+        int expResult = 3;
         int result = instance.mayor(a, b);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
     /**
